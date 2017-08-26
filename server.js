e78da930f9837af8789d47452009eb6aa64babdd
chirag-11 <chirag.ag11@gmail.com>
2017-08-26 12:59:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.use(bp.urlencoded({
   extended: true
 }));
 if (process.env.NODE_ENV == 'production') {
-  console.log(process.env.NODE_ENV);
   config = require('./webpack.config.prod')
 } else {
   config = require('./webpack.config')
